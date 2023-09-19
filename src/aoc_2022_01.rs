9eb2ebd6a8351e508f38bf2ec9d0f8123ccdc5ce
achuvyas <iachuvyasvak@gmail.com>
2023-09-20 02:28:14 +0530

    aoc01, part 2

--- src/aoc_2022_01.rs
+++ src/aoc_2022_01.rs
@@ -1,17 +1,52 @@
 use std::io::Result;
 use std::str::FromStr;
-use itertools::Itertools;
 
-fn read_one_per_line<T>(path: &str) -> Result<Vec<T>>
+fn read_chunks<T>(path: &str) -> Result<Vec<u32>>
 where
     T: FromStr,
 {
-    Ok(std::fs::read_to_string(path)?
-        .lines()
-        .filter_map(|line| line.parse::<T>().ok())
-        .collect())
+    let contents = std::fs::read_to_string(path)?;
+    let mut chunk_sum = 0;
+    let mut sums = Vec::new();
+
+    for line in contents.lines() {
+        if let Ok(value) = line.parse::<u32>() {
+            chunk_sum += value;
+        } else {
+            // When an empty line is encountered, add the accumulated sum to the vector
+            if !line.is_empty() {
+                return Err(std::io::Error::new(
+                    std::io::ErrorKind::InvalidData,
+                    "Invalid input format",
+                ));
+            }
+            sums.push(chunk_sum);
+            chunk_sum = 0;
+        }
+    }
+
+    // Add the final accumulated sum to the vector
+    sums.push(chunk_sum);
+
+    Ok(sums)
 }
 
-pub fn max_calorie() -> Result<usize> {
-    Ok(read_one_per_line::<u32>("./data/aoc202201.txt")?.
+pub fn max_calorie() -> Result<u32> {
+    let chunk_sums = read_chunks::<u32>("./data/aoc202201.txt")?;
+
+    // Create a sorted vector of chunk_sums in descending order
+    let mut sorted_sums: Vec<u32> = chunk_sums.iter().cloned().collect();
+    sorted_sums.sort_by(|a, b| b.cmp(a));
+
+    // Take the top 3 maximum values and sum them
+    let sum_of_top3: u32 = sorted_sums.iter().take(3).sum();
+
+    if sum_of_top3 == 0 {
+        return Err(std::io::Error::new(
+            std::io::ErrorKind::Other,
+            "No calorie values found.",
+        ));
+    }
+
+    Ok(sum_of_top3)
 }
